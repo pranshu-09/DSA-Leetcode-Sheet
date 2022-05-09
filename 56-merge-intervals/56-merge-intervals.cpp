137a7f538c5e1f1cecda1f37fec50895a3d428f3
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-10 01:14:16 +0530

    Time: 47 ms (59.77%), Space: 19 MB (67.40%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,11 +1,5 @@
-// bool cmp(vector<int>a, vector<int>b){
-//     if(a[1] == b[1]){
-//         return a[0] < b[0];
-//     }
-//     else{
-//         return a[1] < b[1];
-//     }
-// }
+// TC : O(N)
+// SC : O(N)
 
 class Solution {
     
@@ -19,16 +13,13 @@ public:
         
         ans.push_back(intervals[0]);
         
-        int prev_end = intervals[0][1];
-        
         for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0] <= prev_end){
+            if(intervals[i][0] <= ans.back()[1]){
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
             else{
                 ans.push_back(intervals[i]);
             }
-            prev_end = ans.back()[1];
         }
         
         return ans;
