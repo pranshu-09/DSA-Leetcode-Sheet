4f0a5db69aee762b005321ebf7864773ac5ecffd
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-10 23:23:50 +0530

    Time: 325 ms (73.81%), Space: 8.1 MB (37.08%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     
-    bool check(vector<vector<char>>& board, int i, int j, int n, int m, int idx, string&word, vector<vector<bool>>&vis){
+    bool check(vector<vector<char>>& board, int i, int j, int n, int m, int idx, string&word){
         
         if(idx == word.size()){
             return true;
         }
         
-        if(i<0 or j<0 or i>=n or j>=m or vis[i][j] or board[i][j]!=word[idx]){
+        if(i<0 or j<0 or i>=n or j>=m or board[i][j]!=word[idx] or board[i][j]=='_'){
             return false;
         }
         
-        vis[i][j] = true;
+        char ch = board[i][j];        
+        board[i][j] = '_';
         
-        if(board[i][j]==word[idx]){
-            bool up = check(board, i-1, j, n, m, idx+1, word, vis);
-            bool left = check(board, i, j-1, n, m, idx+1, word, vis);
-            bool down = check(board, i+1, j, n, m, idx+1, word, vis);
-            bool right = check(board, i, j+1, n, m, idx+1, word, vis);
-            
-            if(left or right or up or down){
-                return true;
-            }
+        bool up = check(board, i-1, j, n, m, idx+1, word);
+        bool left = check(board, i, j-1, n, m, idx+1, word);
+        bool down = check(board, i+1, j, n, m, idx+1, word);
+        bool right = check(board, i, j+1, n, m, idx+1, word);
+
+        board[i][j] = ch;
+        
+        if(left or right or up or down){
+            return true;
         }
-        
-        vis[i][j] = false;
+
         return false;
     }
     
@@ -35,10 +35,8 @@ public:
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j] == word[0]){
-                    vector<vector<bool>>vis(n, vector<bool>(m, false));
-                    
-                    if(check(board, i, j, n, m, 0, word, vis)){
+                if(board[i][j] == word[0]){                    
+                    if(check(board, i, j, n, m, 0, word)){
                         return true;
                     }
                 }
