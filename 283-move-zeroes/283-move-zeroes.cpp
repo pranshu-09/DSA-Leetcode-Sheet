9b9ba330b8b022929b300b1aeab21112615da8db
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-10 18:16:37 +0530

    Time: 18 ms (94.77%), Space: 19.3 MB (58.89%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,20 @@
+// TC : O(N)
+// SC : O(1)
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int idx = 0;
+        
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != 0){
+                nums[idx++] = nums[i];
+            }
+        }
+        
+        while(idx < nums.size()){
+            nums[idx++] = 0;
+        }
+    }
+};
