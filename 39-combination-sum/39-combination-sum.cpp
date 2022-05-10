8633546c31db2f12cbbd33bc097e3a2727ff3109
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-11 01:11:36 +0530

    Time: 326 ms (6.79%), Space: 79.7 MB (9.29%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,37 +1,71 @@
+// Unbounded Knapsack type approach
 class Solution {
 public:
     
-    set<vector<int>>s;
+    vector<vector<int>>ans;
     
-    void combination(vector<int>& candidates, int target, int sum, vector<int>cur){
+    void combination(vector<int>& candidates, int target, int sum, int i, vector<int>cur){
         
         if(sum == target){
-            sort(cur.begin(), cur.end());
-            s.insert(cur);
+            ans.push_back(cur);
             return;
         }
         
-        if(sum > target){
+        if(i==candidates.size() or sum > target){
             return;
         }
         
-        for(auto x : candidates){
-            if(sum + x <= target){
-                cur.push_back(x);
-                combination(candidates, target, sum+x, cur);
-                cur.pop_back();
-            }
-        }
+        cur.push_back(candidates[i]);
+        combination(candidates, target, sum+candidates[i], i, cur);
+        
+        cur.pop_back();
+        combination(candidates, target, sum, i+1, cur);
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         
-        s.clear();
+        ans.clear();
         
-        combination(candidates, target, 0, {});
-        
-        vector<vector<int>>ans(s.begin(), s.end());
+        combination(candidates, target, 0, 0, {});
         
         return ans;
     }
 };
+
+// class Solution {
+// public:
+    
+//     set<vector<int>>s;
+    
+//     void combination(vector<int>& candidates, int target, int sum, vector<int>cur){
+        
+//         if(sum == target){
+//             sort(cur.begin(), cur.end());
+//             s.insert(cur);
+//             return;
+//         }
+        
+//         if(sum > target){
+//             return;
+//         }
+        
+//         for(auto x : candidates){
+//             if(sum + x <= target){
+//                 cur.push_back(x);
+//                 combination(candidates, target, sum+x, cur);
+//                 cur.pop_back();
+//             }
+//         }
+//     }
+    
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+//         s.clear();
+        
+//         combination(candidates, target, 0, {});
+        
+//         vector<vector<int>>ans(s.begin(), s.end());
+        
+//         return ans;
+//     }
+// };
