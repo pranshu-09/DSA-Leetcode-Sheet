ebf410f37ad56c9669a8c30e47f06ade78422af0
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-11 00:47:45 +0530

    Time: 8 ms (18.87%), Space: 7.1 MB (86.12%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,30 +1,59 @@
 // TC : O(2^N)
+// SC : O(1)
+// Using Bitmasking
 
 class Solution {
-public:
+public:  
     
-    vector<vector<int>>ans;
-    
-    void make_subsets(vector<int>& nums, int i, vector<int>cur){
+    vector<vector<int>> subsets(vector<int>& nums){
         
-        if(i == nums.size()){
-            ans.push_back(cur);
-            return;
+        int n = nums.size();
+        
+        int sets = 1 << n;
+        
+        vector<vector<int>>ans(sets);
+        
+        for(int i=0;i<sets;i++){
+            for(int j=0;j<n;j++){
+                if((i>>j)&1){
+                    ans[i].push_back(nums[j]);
+                }
+            }
         }
-        
-        cur.push_back(nums[i]);
-        make_subsets(nums, i+1, cur);
-        
-        cur.pop_back();
-        make_subsets(nums, i+1, cur);
-    }
-    
-    vector<vector<int>> subsets(vector<int>& nums) {
-        
-        ans.clear();
-        
-        make_subsets(nums, 0, {});        
         
         return ans;
     }
 };
+
+
+// TC : O(2^N)
+// Using Recursion
+
+// class Solution {
+// public:
+    
+//     vector<vector<int>>ans;
+    
+//     void make_subsets(vector<int>& nums, int i, vector<int>cur){
+        
+//         if(i == nums.size()){
+//             ans.push_back(cur);
+//             return;
+//         }
+        
+//         cur.push_back(nums[i]);
+//         make_subsets(nums, i+1, cur);
+        
+//         cur.pop_back();
+//         make_subsets(nums, i+1, cur);
+//     }
+    
+//     vector<vector<int>> subsets(vector<int>& nums) {
+        
+//         ans.clear();
+        
+//         make_subsets(nums, 0, {});        
+        
+//         return ans;
+//     }
+// };
