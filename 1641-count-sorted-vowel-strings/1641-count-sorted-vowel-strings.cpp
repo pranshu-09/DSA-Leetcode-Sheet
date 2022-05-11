e4c282dfeab8001ee203dd7b7bc650f32aef8722
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-11 13:24:05 +0530

    Time: 0 ms (100.00%), Space: 6 MB (58.60%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,26 +1,58 @@
+// TC : O(N)
+// SC : O(1)
+// DP Approach
+
 class Solution {
 public:
     
-    vector<char>vow{'a','e','i','o','u'};
-    
-    int count_vowel(int n, char ch){
+    int countVowelStrings(int n) {
         
-        if(n==0){
-            return 1;
+        vector<int>dp(5, 1);
+        
+        for(int i=2;i<=n;i++){
+            
+            // Start updating from right to left
+            // Value for string starting with 'u' will always be 1 for lexicographically sorted ans
+            for(int j=3;j>=0;j--){
+                // dp[i] = old dp[i] value + new updated dp[i+1] value
+                dp[j] = dp[j] + dp[j+1];
+            }
         }
         
         int ans = 0;
-        
-        for(int i=0;i<5;i++){
-            if(ch <= vow[i]){
-                ans += count_vowel(n-1, vow[i]);
-            }
+        for(auto x : dp){
+            ans += x;
         }
         
         return ans;
     }
-    
-    int countVowelStrings(int n) {
-        return count_vowel(n,'a');
-    }
 };
+
+
+// Recursive Apprach
+// class Solution {
+// public:
+    
+//     vector<char>vow{'a','e','i','o','u'};
+    
+//     int count_vowel(int n, char ch){
+        
+//         if(n==0){
+//             return 1;
+//         }
+        
+//         int ans = 0;
+        
+//         for(int i=0;i<5;i++){
+//             if(ch <= vow[i]){
+//                 ans += count_vowel(n-1, vow[i]);
+//             }
+//         }
+        
+//         return ans;
+//     }
+    
+//     int countVowelStrings(int n) {
+//         return count_vowel(n,'a');
+//     }
+// };
