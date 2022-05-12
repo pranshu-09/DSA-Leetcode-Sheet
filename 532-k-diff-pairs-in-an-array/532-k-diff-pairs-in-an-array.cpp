004a2885829075ae47c712764e2a1870c1985b98
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-13 00:56:44 +0530

    Time: 17 ms (80.08%), Space: 11.2 MB (80.59%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,6 +1,52 @@
+// TC : O(Nlog(N))
+// SC : O(1)
+
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        
+        sort(nums.begin(), nums.end());
+        
+        int n = nums.size();
+        int i=0, j=1;
+        
+        int count = 0;
+        
+        while(j<n){
+            
+            if(i==j or nums[j]-nums[i]<k){
+                j++;
+            }
+            else{
+                if(nums[j]-nums[i] == k){
+                    
+                    count++;
+                    int val = nums[j];
+                    
+                    while(j<n and nums[j]==val){
+                        j++;
+                    }
+                    
+                    if(j == n){
+                        return count;
+                    }
+                }
+                
+                int val = nums[i];
+                while(i<j and nums[i]==val){
+                    i++;
+                }
+            } 
+        }
+        
+        return count;
+    }
+};
+
 // TC : O(N)
 // SC : O(N)
 
+/*
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
@@ -30,3 +76,4 @@ public:
         return count;
     }
 };
+*/
