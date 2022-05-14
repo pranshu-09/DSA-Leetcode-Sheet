353fec4d823f16237e3a97f56b60e0f54f3a4c0a
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-14 13:04:39 +0530

    Time: 134 ms (76.76%), Space: 44.1 MB (19.30%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    
+    int find_time(unordered_map<int, vector<pair<int, int>>>&adj, int k, vector<bool>&vis){
+        
+        int max_time = 0;
+        
+        vis[k] = true;
+        
+        for(auto nbr : adj[k]){
+            if(!vis[nbr.first]){
+                int time = find_time(adj, nbr.first, vis) + nbr.second;
+                max_time = max(max_time, time);
+            }
+        }
+        
+        return max_time;
+    }
+    
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        
+        unordered_map<int, vector<pair<int, int>>>adj;
+        unordered_map<int, int>dist;
+        
+        for(int i=1;i<=n;i++){
+            dist[i] = INT_MAX;
+        }
+        
+        for(auto x : times){
+            adj[x[0]].push_back({x[1], x[2]});
+        }
+        
+        set<pair<int, int>>s;
+        s.insert({0, k});
+        
+        dist[k] = 0;
+        
+        while(!s.empty()){
+            
+            auto p = *(s.begin());
+            
+            int node = p.second;
+            int dis = p.first;
+            s.erase(s.begin());
+            
+            for(auto child : adj[node]){
+                
+                if(child.second + dis < dist[child.first]){
+                    
+                    auto f = s.find({dist[child.first], child.first});
+                    if(f != s.end()){
+                        s.erase(f);
+                    }
+                    
+                    dist[child.first] = child.second + dis;
+                    s.insert({dist[child.first], child.first});
+                }
+            }
+        }
+        
+        int max_time = 0;
+        for(auto x : dist){
+            if(x.second == INT_MAX) return -1;
+            max_time = max(max_time, x.second);
+        }
+        
+        // if(max_time==0) return -1;
+        
+        return max_time;
+    }
+};
