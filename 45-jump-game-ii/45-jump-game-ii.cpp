30ed456224bedb23fa756fde49de03f096d0b85c
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-14 18:24:14 +0530

    Time: 22 ms (66.28%), Space: 16.6 MB (51.87%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,6 +1,39 @@
+// TC : O(N)
+// SC : O(1)
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+        int n = nums.size();
+        int jump = 0;
+        
+        // make a window [l..r] and find the farthest we can go from this window
+        // The next window will start from r+1 and end at farthest index.
+        // By the end, we will reach the destination (last index)
+        int l=0, r=0;
+        
+        while(r<n-1){
+            
+            int farthest = 0;
+            for(int i=l;i<=r;i++){
+                farthest = max(farthest, nums[i]+i);
+            }
+            
+            l = r+1;
+            r = farthest;
+            jump++;        
+        }
+        
+        return jump;
+    }
+};
+
+
 // TC : O(N^2)
 // SC : O(N)
 
+/*
 class Solution {
 public:
     int jump(vector<int>& nums) {
@@ -25,3 +58,4 @@ public:
         return dp[0];
     }
 };
+*/
