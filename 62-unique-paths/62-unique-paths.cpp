896ffe83e8286416ee3a78f29d38c25e1fe4dabe
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-15 23:47:22 +0530

    Time: 6 ms (9.73%), Space: 6 MB (74.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,6 +1,34 @@
 // TC : O(MxN)
+// SC : O(N)
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+        int dp[n];
+        
+        for(int i=0;i<n;i++){
+            dp[i] = 1;
+        }
+        
+        // Here, we can reduce SC from O(NxM) to O(N) as for dp[i][j], we were adding dp[i-1][j] and dp[i][j-1] in it. 
+        // We can observe that dp[i-1][j] will be stored at dp[j] itself. we are just adding dp[j-1] in dp[j]. Hence, SC can be reduced.
+        
+        for(int j=1;j<m;j++){
+            for(int i=1;i<n;i++){
+                dp[i] = dp[i] + dp[i-1];
+            }
+        }
+        
+        return dp[n-1];
+    }
+};
+
+
+// TC : O(MxN)
 // SC : O(MxN)
 
+/*
 class Solution {
 public:
     int uniquePaths(int m, int n) {
@@ -24,3 +52,4 @@ public:
         return dp[m-1][n-1];
     }
 };
+*/
