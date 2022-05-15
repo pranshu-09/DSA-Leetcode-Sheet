682d0b22eac94e392e10d7190361cf64ba60908f
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-15 22:46:45 +0530

    Time: 5 ms (18.41%), Space: 7.6 MB (76.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,5 +1,31 @@
 // TC : O(N)
+// SC : O(1)
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        if(n==1){
+            return nums[0];
+        }
+        
+        int a = nums[0];
+        int b = max(nums[0], nums[1]);
+        
+        for(int i=2;i<n;i++){
+            int c = max(b, nums[i]+a);
+            a = b;
+            b = c;
+        }
+        
+        return b;
+    }
+};
+
+// TC : O(N)
 // SC : O(N)
+/*
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -22,3 +48,4 @@ public:
         return dp[n-1];
     }
 };
+*/
