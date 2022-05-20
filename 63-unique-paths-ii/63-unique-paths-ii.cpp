52526bd71690d7e075d0a806a26f0daf7a70f27c
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-20 22:07:42 +0530

    Time: 5 ms (40.74%), Space: 7.5 MB (98.16%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,3 +1,47 @@
+// TC : O(NxM)
+// SC : O(NxM)
+class Solution {
+public:
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        
+        int dp[n][m];
+        memset(dp, 0, sizeof dp);
+        
+        if(obstacleGrid[0][0]==0) dp[0][0] = 1;
+        
+        for(int i=1;i<n;i++){
+            if(obstacleGrid[i][0]==0 and dp[i-1][0]!=0){
+                dp[i][0] = 1;
+            }
+        }
+        
+        
+        for(int i=1;i<m;i++){
+            if(obstacleGrid[0][i]==0 and dp[0][i-1]!=0){
+                dp[0][i] = 1;
+            }
+        }
+        
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(obstacleGrid[i][j]==0){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]; 
+                }
+            }
+        }
+        
+        return dp[n-1][m-1];        
+    }
+};
+
+
+// TC : O(NxM)
+// SC : O(NxM)
+/*
 class Solution {
 public:
     
@@ -38,3 +82,4 @@ public:
         return find_paths(obstacleGrid, 0, 0);        
     }
 };
+*/
