54b6583a3974e960bcea2f18b7fac51a942b1dbb
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-22 23:08:57 +0530

    Time: 1038 ms (5.24%), Space: 413.5 MB (5.75%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -0,0 +1,94 @@
+// Memoization
+// TC : O(N^2)
+// SC : O(N^2)
+class Solution {
+public:
+    
+    int dp[1001][1001];
+    
+    bool check_palin(string s, int i, int j){
+        
+        if(i==s.size()){
+            return false;
+        }
+        
+        if(i>=j){
+            return true;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        if(s[i]==s[j] and check_palin(s, i+1, j-1)){
+            return dp[i][j] = true;
+        }
+    
+        return dp[i][j] = false;        
+    }
+    
+    int countSubstrings(string s) {
+        
+        int n = s.size();
+        
+        memset(dp, -1, sizeof dp);
+        
+        int ans = n;
+        
+        for(int k=2;k<=n;k++){
+            for(int i=0;i<=n-k;i++){
+                int j = i+k-1;
+                
+                if(check_palin(s, i, j)){
+                    ans++;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+// Recursive Solution
+/*
+class Solution {
+public:
+    
+    bool check_palin(string s, int i, int j){
+        
+        if(i==s.size()){
+            return false;
+        }
+        
+        if(i>=j){
+            return true;
+        }
+        
+        if(s[i]==s[j] and check_palin(s, i+1, j-1)){
+            return true;
+        }
+    
+        return false;        
+    }
+    
+    int countSubstrings(string s) {
+        
+        int n = s.size();
+        
+        int ans = n;
+        
+        for(int k=2;k<=n;k++){
+            for(int i=0;i<=n-k;i++){
+                int j = i+k-1;
+                
+                if(check_palin(s, i, j)){
+                    ans++;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+*/
