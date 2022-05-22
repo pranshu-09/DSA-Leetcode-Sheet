2f480ec218b477396fcea525fa18933c36aae7c2
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-22 23:35:44 +0530

    Time: 38 ms (27.10%), Space: 20.2 MB (18.18%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,3 +1,41 @@
+// TC : O(N^2)
+// SC : O(N^2)
+class Solution {
+public:
+    
+    int countSubstrings(string s) {
+        
+        int n = s.size();
+        
+        vector<vector<int>>dp(n, vector<int>(n, 0));
+        
+        int ans = 0;
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(i==j){
+                    dp[i][j] = 1;
+                }
+                else if(i+1 == j){
+                    dp[i][j] = s[i]==s[j] ? 1 : 0;
+                }
+                else{
+                    if(s[i]==s[j]){
+                        dp[i][j] = dp[i+1][j-1];
+                    }
+                    else{
+                        dp[i][j] = 0;
+                    }
+                }
+                ans += dp[i][j];
+            }
+        }
+        
+        return ans;
+    }
+};
+
+/*
 // Memoization
 // TC : O(N^2)
 // SC : O(N^2)
@@ -48,6 +86,7 @@ public:
         return ans;
     }
 };
+*/
 
 
 // Recursive Solution
