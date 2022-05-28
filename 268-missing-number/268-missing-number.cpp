c7c4c260a231a5b2147540c8b62cf5e0c5a490e4
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-05-28 23:53:38 +0530

    Time: 39 ms (18.86%), Space: 18 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -4,6 +4,31 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         
+        nums.push_back(0);
+        
+        for(int i=0;i<nums.size();i++){
+            while(nums[i] != nums[nums[i]]){
+                swap(nums[i], nums[nums[i]]);
+            }            
+        }
+        
+        for(int i=0;i<nums.size();i++){
+            if(i != nums[i]){
+                return i;
+            }
+        }
+        
+        return 0;
+    }
+};
+
+// TC : O(N)
+// SC : O(1)
+/*
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
         int n = nums.size();
         
         int xorr = 0;
@@ -19,3 +44,4 @@ public:
         return xorr;
     }
 };
+*/
