e4724bc57d1e436dd4dc950ead87e77acc0afff7
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-06-03 23:52:33 +0530

    Time: 439 ms (90.15%), Space: 149.4 MB (17.79%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,6 +1,44 @@
-// TC : O(NxM) for constructor    O(N) for sumRegion
+// TC : O(NxM) for constructor    O(1) for sumRegion
 // SC : O(NxM)
 class NumMatrix {
+public:
+    
+    int dp[201][201];
+    
+    NumMatrix(vector<vector<int>>& matrix) {
+        
+        memset(dp, 0, sizeof dp);
+        int n = matrix.size(), m = matrix[0].size();
+        
+        dp[1][1] = matrix[0][0];
+        
+        for(int i=2;i<=m;i++){
+            dp[1][i] = dp[1][i-1] + matrix[0][i-1]; 
+        }
+        
+        for(int i=2;i<=n;i++){
+            dp[i][1] = dp[i-1][1] + matrix[i-1][0]; 
+        }
+        
+        for(int i=2;i<=n;i++){
+            for(int j=2;j<=m;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + matrix[i-1][j-1];
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        
+        int ans = dp[row2+1][col2+1] - dp[row1][col2+1] - dp[row2+1][col1] + dp[row1][col1];
+        
+        return ans;
+    }
+};
+
+
+// TC : O(NxM) for constructor    O(N) for sumRegion
+// SC : O(NxM)
+/*class NumMatrix {
 public:
     
     int dp[201][201];
@@ -33,6 +71,7 @@ public:
         return ans;
     }
 };
+*/
 
 /**
  * Your NumMatrix object will be instantiated and called as such:
