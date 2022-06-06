adb9e294c9ddfde9d9d061b4d6fbb36cce50f3f2
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-06-06 13:25:53 +0530

    Time: 13 ms (56.19%), Space: 7.6 MB (96.88%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/227-basic-calculator-ii.cpp
@@ -1,4 +1,56 @@
 // TC : O(N)
+// SC : O(1)
+
+class Solution {
+public:
+    int calculate(string s) {
+        
+        if(s.size()==0) return 0;
+        
+        int n = s.size();
+        
+        int cur = 0, last = 0;
+        char oper = '+';
+        
+        int sum = 0;
+        
+        for(int i=0;i<n;i++){
+            
+            if(s[i]>='0' and s[i]<='9'){
+                cur = cur*10 + (s[i]-'0');
+            }
+            
+            if(!(s[i]>='0' and s[i]<='9') and (s[i]!=' ') or i==n-1){
+                
+                if(oper=='+'){
+                    sum += last;
+                    last = cur;
+                }
+                else if(oper=='-'){
+                    sum += last;
+                    last = -cur;
+                }
+                else if(oper=='*'){
+                    last = last * cur;
+                }
+                else if(oper=='/'){
+                    last = last / cur;
+                }
+                
+                oper = s[i];
+                cur = 0;
+            }
+        }
+        
+        sum += last;
+        
+        return sum;
+    }
+};
+
+
+/*
+// TC : O(N)
 // SC : O(N)
 
 class Solution {
@@ -52,3 +104,4 @@ public:
         return sum;
     }
 };
+*/
