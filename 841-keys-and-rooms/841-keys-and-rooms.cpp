15f9704e7596e6de191399fe9df2b1c637fe546f
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-06-17 22:48:34 +0530

    Time: 13 ms (58.09%), Space: 10.4 MB (55.14%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     
-    unordered_map<int, list<int>>adj;
-    
-    void open_rooms(int src, vector<bool>&vis){
+    void open_rooms(vector<vector<int>>& rooms, int src, vector<bool>&vis){
         
         vis[src] = true;
         
-        for(auto nbr : adj[src]){
+        for(auto nbr : rooms[src]){
             if(!vis[nbr]){
-                open_rooms(nbr, vis);
+                open_rooms(rooms, nbr, vis);
             }
         }
     }
@@ -17,17 +15,10 @@ public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         
         int n = rooms.size();
-        adj.clear();
-        
-        for(int i=0;i<n;i++){
-            for(auto x : rooms[i]){
-                adj[i].push_back(x);
-            }
-        }
         
         vector<bool>vis(n, false);
         
-        open_rooms(0, vis);
+        open_rooms(rooms, 0, vis);
         
         for(auto x : vis){
             if(x == false){
