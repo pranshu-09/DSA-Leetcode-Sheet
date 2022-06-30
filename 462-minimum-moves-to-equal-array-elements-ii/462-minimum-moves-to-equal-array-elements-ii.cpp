fa2c762e67701f870fb9074de24b2632fe880724
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-06-30 23:43:22 +0530

    Time: 11 ms (86.97%), Space: 10.9 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -8,6 +8,38 @@ public:
         
         sort(nums.begin(), nums.end());
         
+        // 0 0 1 6 8
+        // i       j
+        
+        // i should be equal to j
+        // add something (x) in 0 and subtract something (y) from 8 to make them equal
+        // 0+x = 8-y => x+y = 8-0 => add 8 in the ans => do i++, j--
+        
+        int i=0, j=n-1;
+        int ans = 0;
+        
+        while(i < j){
+            ans += (nums[j]-nums[i]);
+            i++;
+            j--;
+        }
+        
+        return ans;
+    }
+};
+
+
+/*
+// TC : O(NlogN)
+// SC : O(1)
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        
         int median = (n&1) ? nums[n/2] : (nums[n/2] + nums[n/2 - 1])/2;
         
         int ans = 0;
@@ -18,3 +50,4 @@ public:
         return ans;
     }
 };
+*/
