b48c8bf2c3910abf6f865ed2a0fe4a0aeb17c67e
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-07-09 02:31:37 +0530

    Time: 239 ms (88.94%), Space: 71.3 MB (41.06%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,31 @@
+// TC : O(N)
+// SC : O(N)
+typedef pair<int, pair<int, int>> pp;
+
+class Solution {
+public:
+    
+    int dis(int x, int y){
+        return x*x + y*y;
+    }
+    
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        
+        priority_queue<pp, vector<pp>, greater<pp>> pq;
+        
+        for(auto x : points){
+            pq.push({dis(x[0], x[1]), {x[0], x[1]}});
+        }
+        
+        vector<vector<int>>ans;
+        
+        while(!pq.empty() and k--){
+            pp t = pq.top();
+            pq.pop();
+            
+            ans.push_back({t.second.first, t.second.second});
+        }
+        
+        return ans;        
+    }
+};
