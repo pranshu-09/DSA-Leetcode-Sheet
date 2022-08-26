85cb05ec387fe1e07f60cef974137abc90c29dc3
Pranshu Bansal <66867334+pranshu-09@users.noreply.github.com>
2022-08-26 23:13:02 +0530

    Time: 3 ms (73.16%), Space: 6.2 MB (55.65%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    long counter(int n) {
+        long res = 0;
+        
+        for (; n; n/=10){
+            res += pow(10, n%10);  
+        } 
+        
+        return res;
+    }
+    
+    bool reorderedPowerOf2(int n) {
+        
+        long c = counter(n);
+        
+        for (int i = 0; i < 32; i++){
+            if (counter(1 << i) == c){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
